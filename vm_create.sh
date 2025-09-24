9486d6d3b52b10f4379ad57cfa256ce88bceadf5
Gonzalo Manuel Álvarez García <153403689+OhMondayMonday@users.noreply.github.com>
2025-09-23 22:03:42 -0500

    FInal VM creation

--- vm_create.sh
+++ vm_create.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Script para crear una VM usando la imagen base cirros-0.5.1-x86_64-disk.img y conectar su TAP al OVS con VLAN.
-# Uso: ./vm_create.sh <nombreVM> <nombreOvS> <VLAN_ID> <VNC_PORT>
-# Ejemplo: ./vm_create.sh VM1 br-int 100 5901
-
 if [ "$#" -ne 4 ]; then
   echo "Uso: $0 <nombreVM> <nombreOvS> <VLAN_ID> <VNC_PORT>"
   exit 1
@@ -13,35 +9,34 @@ NOMBRE_VM="$1"
 NOMBRE_OVS="$2"
 VLAN_ID="$3"
 VNC_PORT="$4"
-
 TAP_IF="tap_${NOMBRE_VM}"
 
-# Ruta a la imagen base Cirros (ajusta si usas otra ubicación)
+# Ruta a la imagen base Cirros
 DISK_PATH="/var/lib/libvirt/images/cirros-0.5.1-x86_64-disk.img"
-
 if [ ! -f "$DISK_PATH" ]; then
   echo "ERROR: No se encuentra la imagen base en $DISK_PATH"
   exit 2
 fi
 
-# Crear interfaz TAP para la VM
-sudo ip tuntap add dev $TAP_IF mode tap
-sudo ip link set $TAP_IF up
+# Crear interfaz TAP (si no existe)
+if ! ip link show "$TAP_IF" &>/dev/null; then
+  sudo ip tuntap add dev $TAP_IF mode tap
+  sudo ip link set $TAP_IF up
+fi
 
 # Conectar TAP al OVS y asignar VLAN
-sudo ovs-vsctl add-port $NOMBRE_OVS $TAP_IF
+sudo ovs-vsctl --may-exist add-port $NOMBRE_OVS $TAP_IF
 sudo ovs-vsctl set port $TAP_IF tag=$VLAN_ID
 
-# Crear la VM usando la imagen base Cirros
-sudo virt-install \
-  --name "$NOMBRE_VM" \
-  --ram 256 \
-  --vcpus 1 \
-  --disk path="$DISK_PATH",format=qcow2,readonly=on \
-  --import \
-  --os-type=linux \
-  --network tap,ifname=$TAP_IF,script=no,downscript=no \
-  --graphics vnc,port=$VNC_PORT,listen=0.0.0.0 \
-  --noautoconsole
+# Lanzar la VM con QEMU
+sudo qemu-system-x86_64 \
+  -name "$NOMBRE_VM" \
+  -m 256 \
+  -smp 1 \
+  -hda "$DISK_PATH" \
+  -netdev tap,id=net0,ifname=$TAP_IF,script=no,downscript=no \
+  -device virtio-net-pci,netdev=net0 \
+  -vnc :$((VNC_PORT-5900)) \
+  -daemonize
 
-echo "VM $NOMBRE_VM creada y conectada a $NOMBRE_OVS (VLAN $VLAN_ID, VNC $VNC_PORT, usando imagen Cirros)"
+echo "VM $NOMBRE_VM lanzada en QEMU, conectada a $NOMBRE_OVS (VLAN $VLAN_ID, VNC $VNC_PORT)"
